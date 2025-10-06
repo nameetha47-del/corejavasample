603e13d5a83d62bf3de0943c77a01587f6584afa
nameetha47-del <nameetha47@gmail.com>
2025-10-06 22:16:00 +0530

    This My Day 4 Java projec Using first package and second package

---
+++ corejavasample/src/day4/secondpackage/Executor.java
@@ -0,0 +1,13 @@
+package day4.secondpackage;
+import day4.firstpackage.Base;
+
+public class Executor {
+	public static void main(String[] args) {
+		// accessing different package class
+		Base b1 = new Base();
+
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
+	}
+}
