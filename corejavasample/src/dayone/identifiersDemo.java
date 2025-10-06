61cecb1016a1a263fb0b6f66f52fdf9c3fe4fc6f
nameetha47-del <nameetha47@gmail.com>
2025-10-06 19:59:42 +0530

    This my Day One Java Project

---
+++ corejavasample/src/dayone/identifiersDemo.java
@@ -0,0 +1,13 @@
+package dayone;
+
+public class identifiersDemo {
+	
+	public static void main(String[] args) {
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Aniket";
+		System.out.println("value is : "+ studentName);
+
+	}
+
+}
