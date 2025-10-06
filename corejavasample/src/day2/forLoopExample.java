d131b34f20ab5328953bce190343d2e736315294
nameetha47-del <nameetha47@gmail.com>
2025-10-06 20:38:17 +0530

    This my Day 2 Java Project

---
+++ corejavasample/src/day2/forLoopExample.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class forLoopExample {
+
+	public static void main(String[] args) {
+			for(int i = 1; i<=1000 ; i++)   
+			{	
+				System.out.print("Value of i:");
+				System.out.println(i);
+			}
+
+
+	}
+
+}
