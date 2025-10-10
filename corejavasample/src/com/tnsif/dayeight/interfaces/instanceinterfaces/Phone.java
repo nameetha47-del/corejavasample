cb5fc2fd478e134a5a89b6f9c1d85799cdee8681
nameetha47-del <nameetha47@gmail.com>
2025-10-10 22:08:30 +0530

    This my day-8 java project

---
+++ corejavasample/src/com/tnsif/dayeight/interfaces/instanceinterfaces/Phone.java
@@ -0,0 +1,8 @@
+package com.tnsif.dayeight.interfaces.instanceinterfaces;
+
+	public interface Phone {
+		void call();
+
+		void sms();
+	}
+
---
+++ corejavasample/src/com/tnsif/dayeight/interfacesextendinginterfaces/ChildInterface.java
@@ -0,0 +1,5 @@
+package com.tnsif.dayeight.interfacesextendinginterfaces;
+
+public interface ChildInterface extends InterfaceOne {
+	void show();
+}
