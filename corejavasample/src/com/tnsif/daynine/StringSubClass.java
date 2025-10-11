53c08e514830b194d862fad30b63a78729567906
nameetha47-del <nameetha47@gmail.com>
2025-10-11 12:58:11 +0530

    This my day-9 java project

---
+++ corejavasample/src/com/tnsif/daynine/StringSubClass.java
@@ -0,0 +1,13 @@
+//to demonstrate the error
+
+//can't inherit final classes
+package com.tnsif.daynine;
+
+public class StringSubClass {
+
+	public static void main(String[] args) {
+		System.out.println("cannot extend final class String");
+		
+
+	}
+}
